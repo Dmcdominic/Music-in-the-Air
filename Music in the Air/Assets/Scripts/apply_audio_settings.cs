314e1aecb4763466f78c756e7c9c5c30b60d1d76
Dmcdominic <dmcdominic@gmail.com>
2018-10-27 12:59:33 -0400

    Imported LeapMotion Core Assets and began building literal hand-adjusted audio settings.

---
+++ Music in the Air/Assets/Scripts/apply_audio_settings.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class apply_audio_settings : MonoBehaviour {
+
+	public audio_settings audio_Settings;
+
+	private AudioSource AS;
+
+
+	private void Awake() {
+		AS = GetComponent<AudioSource>();
+	}
+
+	private void Update() {
+		if (audio_Settings) {
+			AS.volume = audio_Settings.volume;
+			AS.pitch = audio_Settings.pitch;
+			AS.panStereo = audio_Settings.stereo_pan;
+		}
+	}
+
+}
---
+++ Music in the Air/Assets/Scripts/audio_settings.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName="Custom/audio_settings")]
+public class audio_settings : ScriptableObject {
+
+	[Range(0.0f, 1.0f)]
+	public float volume;
+
+	[Range(-3.0f, 3.0f)]
+	public float pitch;
+
+	[Range(-1.0f, 1.0f)]
+	public float stereo_pan;
+	
+}
