2cbb7aacf08631c86d5b32e666a82e9190603e33
Dmcdominic <dmcdominic@gmail.com>
2018-11-07 21:28:35 -0500

    Channel adjusted audio added

---
+++ Music in the Air/Assets/Scripts/channel_adjusted_audio.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class channel_adjusted_audio : MonoBehaviour {
+
+	public AudioClip audioClip;
+	public int channel_index;
+
+
+	private void Awake() {
+		AudioClip adjustedAudioClip = audioClip.CreateSpeakerSpecificClip(1, channel_index);
+		AudioSource audioSource = GetComponent<AudioSource>();
+		audioSource.clip = adjustedAudioClip;
+	}
+
+}
